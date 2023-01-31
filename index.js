4bae81db305febe4ff38ac3908d0156109cfb30c
RITHIKA P <91261230+RITHIKA-P@users.noreply.github.com>
2023-01-31 21:35:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const hourE1 = document.getElementById("hour");
-const minutesE1 = document.getElementById("minutes");
-const secondsE1 = document.getElementById("seconds");
+const minuteE1 = document.getElementById("minutes");
+const secondE1 = document.getElementById("seconds");
 const ampmE1 = document.getElementById("ampm");
 function updateClock(){
-    let h = new Date().getHours()
-    let m = new Date().getMinutes()
-    let s = new Date().getSeconds()
-    let ampm = "AM"
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
 
     if (h > 12) {
         h = h-12;
@@ -17,8 +17,8 @@ function updateClock(){
     s = s<10 ? "0" + s : s;
 
     hourE1.innerText = h;
-    minutesE1.innerText = m;
-    secondsE1.innerText = s;
+    minuteE1.innerText = m;
+    secondE1.innerText = s;
     ampmE1,(innerText = ampm);
     setTimeout(()=>{
      updateClock()
