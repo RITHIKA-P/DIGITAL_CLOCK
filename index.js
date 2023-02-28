6b393deafdcf9c6ac37cb40a28fe1196fd19239f
RITHIKA P <91261230+RITHIKA-P@users.noreply.github.com>
2023-02-28 09:57:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ function updateClock(){
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let ampm = "PM";
+    let ampm = "AM";
 
     if (h > 12) {
         h = h-12;
-        ampm = "AM";
+        ampm = "PM";
     }
     h = h<10 ? "0" + h : h;
     m = m<10 ? "0" + m : m;
